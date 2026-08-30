64da09ed4f826ac265e9221f2734153c0bbbe689
coleYab <yabume123@gmail.com>
2026-08-30 11:54:55 +0300

    footer: update social links to current Carelink profiles

--- resources/js/components/carelink/footer.tsx
+++ resources/js/components/carelink/footer.tsx
@@ -7,19 +7,41 @@ import {
     Linkedin,
     Mail,
     Phone,
-    Twitter,
-    Youtube,
 } from 'lucide-react';
 import { useCms, useCompanyInfo } from '@/lib/cms';
 import type { PaymentMethod } from '@/lib/cms';
 import { login, smsTerms } from '@/routes';
 
+function TiktokIcon({ className }: { className?: string }) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            aria-hidden="true"
+            className={className}
+        >
+            <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
+        </svg>
+    );
+}
+
 const socialLinks = [
-    { name: 'Facebook', href: '#', icon: Facebook },
-    { name: 'Instagram', href: '#', icon: Instagram },
-    { name: 'LinkedIn', href: '#', icon: Linkedin },
-    { name: 'Twitter', href: '#', icon: Twitter },
-    { name: 'YouTube', href: '#', icon: Youtube },
+    {
+        name: 'Facebook',
+        href: 'https://web.facebook.com/people/Carelink-Medical-Transport-LLC/61593642704510/',
+        icon: Facebook,
+    },
+    {
+        name: 'Instagram',
+        href: 'https://www.instagram.com/carelinknemt/',
+        icon: Instagram,
+    },
+    {
+        name: 'LinkedIn',
+        href: 'http://linkedin.com/company/carelinknemt',
+        icon: Linkedin,
+    },
+    { name: 'TikTok', href: 'https://www.tiktok.com/@carelinknemt', icon: TiktokIcon },
 ];
 
 export default function Footer() {
