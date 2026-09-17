3926390320f726ab5b0db5cd525dc718aba957c5
coleYab <yabume123@gmail.com>
2026-09-17 19:08:04 +0300

    maps: fix api key required error by switching carto to openstreetmap

--- resources/js/components/carelink/map-preview.tsx
+++ resources/js/components/carelink/map-preview.tsx
@@ -104,8 +104,8 @@ export default function MapPreview({
                     />
                 ) : (
                     <TileLayer
-                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
                 )}
                 <FitPoints points={points} route={route} />
